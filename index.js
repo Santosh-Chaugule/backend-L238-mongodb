9e0154a0be5efab5e14d486c091a29ce429c4b30
santosh-chaugule <santoshchaugule1697@gmail.com>
2023-04-04 14:32:02 +0530

    SECOND

--- index.js
+++ index.js
@@ -2,4 +2,22 @@
 
 //const something =require('somelibrary')
 const express = require('express')
-console.log("Thhis second commit")
+const app = express();
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+async function main() {
+    //every function return something
+    await mongoose.connect('mongodb+srv://santosh:Santosh1993@cluster0.mczqiac.mongodb.net/?retryWrites=true&w=majority')
+}
+
+//po.then().catch((err)=>{})
+main().catch(err => console.log(err))
+
+
+
+
+let port = process.env.PORT || 6000;
+app.listen(3000, () => {
+    console.log('the server is running on' + port)
+})
